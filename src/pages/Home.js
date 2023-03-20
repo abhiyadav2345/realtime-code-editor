62241667e731603dd32d72e795bbe5d89f717ab9
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-03-20 21:11:24 +0530

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-loo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="ROOM ID" />
+          <input type="text" className="inputBox" placeholder="USERNAME" />
+          <button className="btn joinBtn">Join</button>
 
-export default Home
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a href="/" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with ❤️ by <a href="/">Abhishek Yadav</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
