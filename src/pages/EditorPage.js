92d6b7cda335ad54f19d142eb93eb42484b3beab
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-04-01 00:51:34 +0530

    WS error handling

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -3,24 +3,55 @@ import Client from "../components/Client";
 import Editor from "../components/Editor";
 import { initSocket } from "../socket";
 import ACTIONS from "../Actions";
-import { useLocation } from "react-router-dom";
+import {
+  useLocation,
+  reactNavigate,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import { toast } from "react-hot-toast";
 const EditorPage = () => {
   const socketRef = useRef(null);
-
+  const location = useLocation();
+  const reactNavigator = useNavigate();
+  const { roomId } = useParams();
+  const [clients, setClient] = useState([]);
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
-      // socketRef.current.emit(ACTIONS.JOIN, {
-      //   roomId,
-      //   username: location.state?.username,
-      // });
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed ,try again later.");
+        reactNavigator("/");
+      }
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+          }
+         
+
+          setClient(clients);
+        }
+      );
     };
     init();
   }, []);
-  const [clients, setClient] = useState([
-    { socketId: 1, username: "nancy" },
-    { socketId: 2, username: "Harsh" },
-  ]);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className="mainWrap">
       <div className="aside">
