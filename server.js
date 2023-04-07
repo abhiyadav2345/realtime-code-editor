902ed25627118f19c930a4d45a1c1d0603d267d4
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-04-07 09:51:04 +0530

    Port removed

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
