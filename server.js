af97f29cdafe987a4a89ea8318cde0973db75dd5
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-04-07 09:31:58 +0530

    Port changed from 5001 to 8080

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
