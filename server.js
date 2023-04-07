4e6bdc1d6231e8774bad59f9b5deccd1747f7d3f
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-04-07 09:53:39 +0530

    8080 added

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
